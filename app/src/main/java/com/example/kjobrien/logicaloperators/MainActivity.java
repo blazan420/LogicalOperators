1c08b03d1cdaa275d2421bc534051362e9cff051
kevin <kevinjobrien420@gmail.com>
2018-06-07 00:37:00 -0500

    Logical Operators #139 App Number 8 - Part2 - Master using Logical Operators in your apps //Complete

---
+++ app/src/main/java/com/example/kjobrien/logicaloperators/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.kjobrien.logicaloperators;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create Objects
+        TextView txtHealthy = (TextView) findViewById(R.id.txtHealthy);
+        final CheckBox chxHealthyYes = (CheckBox) findViewById(R.id.chxHealthyYes);
+        final CheckBox chxHealthyNo = (CheckBox) findViewById(R.id.chxHealthyNo);
+
+
+        chxHealthyYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (chxHealthyYes.isChecked()) {
+
+                    chxHealthyNo.setChecked(false);
+
+                }
+
+            }
+        });
+
+
+        chxHealthyNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (chxHealthyNo.isChecked()) {
+
+                    chxHealthyYes.setChecked(false);
+                }
+            }
+        });
+
+        boolean amIHealthy = false;
+
+        if (amIHealthy) {
+
+            chxHealthyYes.setChecked(true);
+        } else if (!amIHealthy) {
+            chxHealthyNo.setChecked(true);
+        }
+
+    }
+}
